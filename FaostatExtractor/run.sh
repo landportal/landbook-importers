0c19118f4cd318788eb2ce399757fffe60d3b8ff
Carlos Tejo <carlos.tejo@landportal.info>
2016-02-24 10:47:12 +0000

    updated FAO STAT importer run script adding support for XML schema validation and RDF generation

--- FaostatExtractor/run.sh
+++ FaostatExtractor/run.sh
@@ -1,4 +1,23 @@
-./clean.sh & python main.py
-xmllint --format --output pretty.xml DATFAOSTAT0_1_0.xml
-grep -n "<indicator id=" pretty.xml
-xmllint --noout --schema ../ModelToXml/landportalDataset.xsd pretty.xml
+./clean.sh # this scripts include to remove all .xml files
+
+python main.py #generate the intermediate XML
+
+for file in *.xml
+do
+  # obtain the filename and the extension
+  file_name="${file%.*}"
+  file_extension="${file##*.}"
+
+  # pretty print the intermediate XML
+  xmllint --format --output $file $file
+
+  # check the indicators added
+  grep -n "<indicator id=" $file
+
+  # validate the intermediate XML against the XML Schema
+  xmllint --noout --schema ../ModelToXml/landportalDataset.xsd $file
+
+  # generate the RDF (in RDF/XML and Turtle)
+  python ../../landbook-receiver/rdf-generator.py  -i $file -o $file_name
+done
+
