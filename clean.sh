b022b673ced91a833ed71ccd8bdbde3ab06b2124
Carlos Tejo <carlos.tejo@landportal.info>
2016-06-01 22:49:47 +0000

    updated root scripts to generate RDF and to clean folders

---
+++ clean.sh
@@ -0,0 +1,22 @@
+cd LandMatrix_Importer
+./clean.sh
+
+cd ..
+cd FAOSTAT-Norm_Importer
+./clean.sh
+
+cd ..
+cd FAOSTAT_Importer
+./clean.sh
+
+cd ..
+cd FAOAgriculturalCensues_Importer
+./clean.sh
+
+cd ..
+cd WorldBank_Importer
+./clean.sh
+
+cd ..
+cd Simple_XLS_Importer
+./clean.sh
---
+++ run.sh
@@ -0,0 +1,22 @@
+cd LandMatrix_Importer
+./run.sh
+
+cd ..
+cd FAOSTAT-Norm_Importer
+./run.sh
+
+cd ..
+cd FAOSTAT_Importer
+./run.sh
+
+cd ..
+cd FAOAgriculturalCensues_Importer
+./run.sh
+
+cd ..
+cd WorldBank_Importer
+./run.sh
+
+cd ..
+cd Simple_XLS_Importer
+./run.sh
