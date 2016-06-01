fa50b703bbbead160ae069a6981891887a14e358
Carlos Tejo <carlos.tejo@landportal.info>
2016-06-01 22:48:02 +0000

    added Map of donors data & updated script

--- Simple_XLS_Importer/run.sh
+++ Simple_XLS_Importer/run.sh
@@ -5,6 +5,18 @@ mv UNDP-HDI_1_0.xml UNDP-HDI_index.xml
 python main.py -i data/HDI_for_Ingestion_reduced_consistent_FINAL_HDI_ranking.xls #generate the intermediate XML
 mv UNDP-HDI_1_0.xml UNDP-HDI_ranking.xml
 
+python main.py -i data/Map_of_donors_Concluded_Final_number.xlsx #generate the intermediate XML
+mv DP-MOD-1_1_0.xml map-of-donors-concluded-number.xml
+python main.py -i data/Map_of_donors_Concluded_Final_funding.xlsx #generate the intermediate XML
+mv DP-MOD-1_1_0.xml map-of-donors-concluded-funding.xml
+
+python main.py -i data/Map_of_donors_Ongoing_Final_number.xlsx #generate the intermediate XML
+mv DP-MOD-1_1_0.xml map-of-donors-ongoing-number.xml
+python main.py -i data/Map_of_donors_Ongoing_Final_funding.xlsx #generate the intermediate XML
+mv DP-MOD-1_1_0.xml map-of-donors-ongoing-funding.xml
+
+
+
 for file in *.xml
 do
   # obtain the filename and the extension
