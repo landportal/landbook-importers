b4dcb74b6898697b622e929205b9155f37fdec59
Carlos Tejo <carlos.tejo@landportal.info>
2016-06-06 15:39:11 +0000

    Added support for FAO Land & Gender

--- Simple_XLS_Importer/run.sh
+++ Simple_XLS_Importer/run.sh
@@ -1,5 +1,10 @@
 ./clean.sh # this scripts include to remove all .xml files
 
+#############################
+# GENERATE intermediate XML #
+#############################
+
+
 #python main.py -i data/HDI_for_Ingestion_reduced_consistent_FINAL_HDI_index.xls #generate the intermediate XML
 mv UNDP-HDI_1_0.xml UNDP-HDI_index.xml
 #python main.py -i data/HDI_for_Ingestion_reduced_consistent_FINAL_HDI_ranking.xls #generate the intermediate XML
@@ -15,22 +20,46 @@ mv DP-MOD_1_0.xml map-of-donors-ongoing-number.xml
 #python main.py -i data/Map_of_donors_Ongoing_Final_funding.xlsx #generate the intermediate XML
 mv DP-MOD_1_0.xml map-of-donors-ongoing-funding.xml
 
-python main.py -i data/IFPRI/IFPRI-HDI-Child_Mortality.xlsx #generate the intermediate XML
+#python main.py -i data/IFPRI/IFPRI-HDI-Child_Mortality.xlsx #generate the intermediate XML
 mv IFPRI-DAT-0_1_0.xml IFPRI-HDI-Child_Mortality.xml
 
-python main.py -i data/IFPRI/IFPRI-HDI-Child_Stunting.xlsx #generate the intermediate XML
+#python main.py -i data/IFPRI/IFPRI-HDI-Child_Stunting.xlsx #generate the intermediate XML
 mv IFPRI-DAT-0_1_0.xml IFPRI-HDI-Child_Stunting.xml
 
-python main.py -i data/IFPRI/IFPRI-HDI-Child_Wasting.xlsx #generate the intermediate XML
+#python main.py -i data/IFPRI/IFPRI-HDI-Child_Wasting.xlsx #generate the intermediate XML
 mv IFPRI-DAT-0_1_0.xml IFPRI-HDI-Child_Wasting.xml
 
-python main.py -i data/IFPRI/IFPRI-HDI.xlsx #generate the intermediate XML
+#python main.py -i data/IFPRI/IFPRI-HDI.xlsx #generate the intermediate XML
 mv IFPRI-DAT-0_1_0.xml IFPRI-HDI.xml
 
-python main.py -i data/IFPRI/IFPRI-HDI-Undernourishment.xlsx #generate the intermediate XML
+#python main.py -i data/IFPRI/IFPRI-HDI-Undernourishment.xlsx #generate the intermediate XML
 mv IFPRI-DAT-0_1_0.xml IFPRI-HDI-Undernourishment.xml
 
 
+#####################
+# FAO LAND & GENDER #
+#####################
+
+for file_path in data/FAO-LandAndGender/FAO-LandAndGender*.xlsx
+do
+#  file_path_escaped=$(printf '%q' "$file_path")
+  python main.py -i $file_path #generate the intermediate XML
+
+  # obtain the filename and the extension
+  file=${file_path#data/FAO-LandAndGender/*}
+  file_name="${file%.*}"
+  file_extension="${file##*.}"
+
+  mv FAO-LANDANDGENDER_1_0.xml $file_name.xml
+done
+
+###################################################################
+###################################################################
+###################################################################
+
+################
+# GENERATE RDF #
+################
 
 for file in *.xml
 do
